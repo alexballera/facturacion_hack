33eba23fd15cf03e172329b96d436a359ecaff2d
Alex Ballera <alexballera@gmail.com>
2015-07-20 08:59:50 -0430

    Configurado mi correo gmail como default

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'aballera@4geeks.com.ve'
+  config.mailer_sender = 'alexballera@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
